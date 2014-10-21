1d84ff590ac41c25dfc07603981de4607c3348ae
rickytan <ricky.tan.xin@gmail.com>
2014-10-22 02:33:54 +0800

    download recusively

--- index.js
+++ index.js
@@ -32,63 +32,78 @@ var destDir = './';
 
 var localDir = path.resolve(__dirname, destDir);
 
-Q.fcall(function() {
-	var defer = Q.defer();
+var marker = '';
+
+function downloadFile() {
+	Q.fcall(function() {
+		var defer = Q.defer();
 /*
  * prefix:    可选，object 前缀
  * marker:    可选，列表起始object
  * delimiter: 可选，object分组字符，若'/'为则不列出路径深度大于等于二层的object。
  * maxKeys:   可选，列出的object最大个数
  */
-	oss.listObject({
-	  bucket: bucket,
-	  prefix: '',
-	  marker: '',
-	  delimiter: '',
-	  maxKeys: ''
-	}, function (err, res) {
-		if (err) defer.reject(err);
-		else defer.resolve(res.body.ListBucketResult.Contents);
-	});
-	return defer.promise;
-}).then(function(objects) {
-	return objects.map(function(o) {
-		return o.Key[0];
-	});
-}).then(function(keys) {
-	var promises = [];
-	keys.forEach(function(key) {
-		promises.push(Q(key).then(function(key) {
-			var defer = Q.defer();
-			var fullpath = path.join(localDir, key);
-			var dir = path.dirname(fullpath);
-			if (!fs.existsSync(dir)) {
-				fs.mkdirSync(dir);
-			}
-			var stat = fs.statSync(dir);
-			if (!stat.isDirectory()) {
-				fs.rmdirSync(dir);
-				fs.mkdirSync(dir);
-			}
-			return [key, fullpath];
-		}).spread(function (key, fullpath) {
-			var defer = Q.defer();
-			console.log("Downloading %s", key);
-			oss.getObject({
-				bucket: bucket,
-				object: key,
-				dest: fullpath,
-				headers: {}
-			}, function(err, res) {
-				if (err) defer.reject(err);
-				else defer.resolve(res);
-			});
-			return defer.promise;
-		}), function(e) {
-			console.log(e);
+		oss.listObject({
+		  bucket: bucket,
+		  prefix: '',
+		  marker: marker,
+		  delimiter: '',
+		  maxKeys: '1000'
+		}, function (err, res) {
+			if (err) defer.reject(err);
+			else defer.resolve(res.body.ListBucketResult.Contents);
 		});
+		return defer.promise;
+	}).then(function(objects) {
+		return objects.map(function(o) {
+			return o.Key[0];
+		});
+	}).then(function(keys) {
+		if (keys.length == 1000) {
+			marker = keys[keys.length - 1];
+		} else {
+			marker = '';
+		}
+		var promises = [];
+		keys.forEach(function(key) {
+			promises.push(Q(key).then(function(key) {
+				var defer = Q.defer();
+				var fullpath = path.join(localDir, key);
+				var dir = path.dirname(fullpath);
+				if (!fs.existsSync(dir)) {
+					fs.mkdirSync(dir);
+				}
+				var stat = fs.statSync(dir);
+				if (!stat.isDirectory()) {
+					fs.rmdirSync(dir);
+					fs.mkdirSync(dir);
+				}
+				return [key, fullpath];
+			}).spread(function (key, fullpath) {
+				var defer = Q.defer();
+				console.log("Downloading %s", key);
+				oss.getObject({
+					bucket: bucket,
+					object: key,
+					dest: fullpath,
+					headers: {}
+				}, function(err, res) {
+					if (err) defer.reject(err);
+					else defer.resolve(res);
+				});
+				return defer.promise;
+			}), function(e) {
+				console.log(e);
+			});
+		});
+		return Q.all(promises);
+	}).catch(function(e) {
+		console.log(e);
+	}).done(function() {
+		if (marker.length > 0) {
+			downloadFile();
+		}
 	});
-	return Q.all(promises);
-}).catch(function(e) {
-	console.log(e);
-}).done();
+}
+
+downloadFile();
